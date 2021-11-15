296c4cb3957499a4b2528e44b145524394cc30e1
Sven Himstedt <sven.himstedt@gmx.de>
2021-11-15 22:07:56 +0100

    refactored arduino drive controller

--- brutus_drive/brutus_drive.ino
+++ brutus_drive/brutus_drive.ino
@@ -30,52 +30,78 @@ struct brutus_handle_ * p_brutus = &brutus;
 //
 // command function
 //
-bool cmd_version(void * p_data)
+bool cmd_forward(int cookie, void * p_data)
 {
-    him_logd("%s %s\n", PROJECT_NAME_STRING, PROJECT_VERSION_STRING);
-    return true;
-}
+    int res = 0;
 
-bool cmd_forward(void * p_data)
-{
-    him_logd("cmd_forward\n");
     p_brutus->speed_control_l = p_brutus->motor_l.incrementSpeed(10);
     p_brutus->speed_control_r = p_brutus->motor_r.incrementSpeed(10);
-    return true;
+
+    him_cmd_response_cmd(cookie, res, "cmd_forward %d %d", p_brutus->speed_control_l, p_brutus->speed_control_r);
+    return !res;
 }
 
-bool cmd_backward(void * p_data)
+bool cmd_backward(int cookie, void * p_data)
 {
-    him_logd("cmd_backward\n");
+    int res = 0;
+
     p_brutus->speed_control_l = p_brutus->motor_l.decrementSpeed(10);
     p_brutus->speed_control_r = p_brutus->motor_r.decrementSpeed(10);
-    return true;
+
+    him_cmd_response_cmd(cookie, res, "cmd_backward %d %d", p_brutus->speed_control_l, p_brutus->speed_control_r);
+    return !res;
 }
 
-bool cmd_left(void * p_data)
+bool cmd_left(int cookie, void * p_data)
 {
-    him_logd("cmd_left\n");
+    int res = 0;
+
     p_brutus->speed_control_l = p_brutus->motor_l.decrementSpeed(10);
     p_brutus->speed_control_r = p_brutus->motor_r.incrementSpeed(10);
-    return true;
+
+    him_cmd_response_cmd(cookie, res, "cmd_left %d %d", p_brutus->speed_control_l, p_brutus->speed_control_r);
+    return !res;
 }
 
-bool cmd_right(void * p_data)
+bool cmd_right(int cookie, void * p_data)
 {
-    him_logd("cmd_right\n");
+    int res = 0;
+
     p_brutus->speed_control_l = p_brutus->motor_l.incrementSpeed(10);
     p_brutus->speed_control_r = p_brutus->motor_r.decrementSpeed(10);
-    return true;
+
+    him_cmd_response_cmd(cookie, res, "cmd_right %d %d", p_brutus->speed_control_l, p_brutus->speed_control_r);
+    return !res;
 }
 
-bool cmd_stop(void * p_data)
+bool cmd_stop(int cookie, void * p_data)
 {
-    him_logd("cmd_stop\n");
+    int res = 0;
+
     p_brutus->speed_control_l = p_brutus->motor_l.stop();
     p_brutus->speed_control_r = p_brutus->motor_r.stop();
-    return true;
+
+    him_cmd_response_cmd(cookie, res, "cmd_stop %d %d", p_brutus->speed_control_l, p_brutus->speed_control_r);
+    return !res;
 }
 
+bool cmd_center(int cookie, void * p_data)
+{
+    int res = 0;
+
+    int speed = (p_brutus->speed_control_l + p_brutus->speed_control_r) / 2;
+
+    p_brutus->speed_control_l = p_brutus->motor_l.setSpeed(speed);
+    p_brutus->speed_control_r = p_brutus->motor_r.setSpeed(speed);
+
+    him_cmd_response_cmd(cookie, res, "cmd_center %d %d", p_brutus->speed_control_l, p_brutus->speed_control_r);
+    return !res;
+}
+
+void msg_speed(int res, int speed_l, int speed_r )
+{
+    him_cmd_response_msg("speed", res, true, "%3d %3d", speed_l, speed_r );
+}
 
 
 //
@@ -84,13 +110,37 @@ bool cmd_stop(void * p_data)
 void setup() {
     him_serial_init(57600);
 
+    him_cmd_set_name(PROJECT_NAME_STRING, PROJECT_VERSION_STRING);
     him_cmd_set_echo(true);
-    him_cmd_assign_cmd("version", cmd_version, NULL);
-    him_cmd_assign_cmd("w", cmd_forward,  NULL);
-    him_cmd_assign_cmd("s", cmd_backward, NULL);
-    him_cmd_assign_cmd("a", cmd_left,     NULL);
-    him_cmd_assign_cmd("d", cmd_right,    NULL);
-    him_cmd_assign_cmd("q", cmd_stop,     NULL);
+    him_cmd_assign_cmd("w", cmd_forward,  NULL,
+                        "<speed_ctrl_l> <speed_ctrl_r>",
+                        "",
+                        "Increments left and right motor speed by 10");
+    him_cmd_assign_cmd("s", cmd_backward, NULL,
+                        "<speed_ctrl_l> <speed_ctrl_r>",
+                        "",
+                        "Decrements left and right motor speed by 10");
+    him_cmd_assign_cmd("a", cmd_left,     NULL,
+                        "<speed_ctrl_l> <speed_ctrl_r>",
+                        "",
+                        "Increments/decrements left and right motor  by 10 to turn left");
+    him_cmd_assign_cmd("d", cmd_right,    NULL,
+                        "<speed_ctrl_l> <speed_ctrl_r>",
+                        "",
+                        "Increments/decrements left and right motor  by 10 to turn right");
+    him_cmd_assign_cmd("q", cmd_stop,     NULL,
+                        "<speed_ctrl_l> <speed_ctrl_r>",
+                        "",
+                        "Stops motors");
+    him_cmd_assign_cmd("e", cmd_center,   NULL,
+                        "<speed_ctrl_l> <speed_ctrl_r>",
+                        "",
+                        "Centers left and right motor speed to driver straight forward");
+
+    him_cmd_assign_msg( "speed",
+                        "<speed_l> <speed_r>",
+                        "returns the current speed");
+
 }
 
 
@@ -104,8 +154,9 @@ void loop() {
 
     p_brutus->motor_sensor_l.getIncrement(p_brutus->speed_sensor_l, true);
     p_brutus->motor_sensor_r.getIncrement(p_brutus->speed_sensor_r, true);
-    him_logd("Motor %3d/%3d  Sensor%3d/%3d\n", p_brutus->speed_control_l, p_brutus->speed_control_r, p_brutus->speed_sensor_l, p_brutus->speed_sensor_r);
 
-    delay(10);
+    msg_speed(0, p_brutus->speed_sensor_l, p_brutus->speed_sensor_r );
+
+    delay(70);
 }
 
